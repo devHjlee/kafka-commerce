53bf5070cf77cc3a9cf87e00cdda6ed22774ffee
hyungjae.lee <dlgudwo11@gmail.com>
2025-04-28 16:37:43 +0900

    kafka-commerce init commit

---
+++ src/main/java/com/kafkacommerce/KafkaCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kafkacommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafkacommerce/KafkaCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkacommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
