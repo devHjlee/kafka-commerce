2e72b4482cfe0563d9da6442a44bdaf07d8afb37
lhj <dlgudwo11@gmail.com>
2025-05-05 23:41:15 +0900

    feat(product): Kafka DLT 추가
    - order.created 실패시 DLT 발행 추가

--- kafka-commerce-product/src/main/java/com/kafkacommerce/product/config/StockConsumerConfig.java
+++ kafka-commerce-product/src/main/java/com/kafkacommerce/product/config/StockConsumerConfig.java
@@ -2,13 +2,20 @@ package com.kafkacommerce.product.config;
 
 import com.kafkacommerce.common.kafka.event.OrderCreatedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,7 +35,7 @@ public class StockConsumerConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
-        return new org.springframework.kafka.core.DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
@@ -36,6 +43,43 @@ public class StockConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setCommonErrorHandler(productErrorHandler());
         return factory;
+    }
+
+    // DLT(Dead Letter Topic)에 발행할 메시지 템플릿
+    @Bean
+    @Qualifier("produceTemplate")
+    public KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate(ProducerFactory<String, OrderCreatedEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+    @Bean
+    public KafkaTemplate<String, String> stringKafkaTemplate() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        ProducerFactory<String, String> factory = new DefaultKafkaProducerFactory<>(config);
+        return new KafkaTemplate<>(factory);
+    }
+    // DLT(Dead Letter Topic)에 메시지 발행시 사용할 Producer 설정 세팅
+    @Bean
+    public ProducerFactory<String, OrderCreatedEvent> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    // 에러 핸들러 구현
+    @Bean
+    public CommonErrorHandler productErrorHandler() {
+        DeadLetterPublishingRecoverer recover = new DeadLetterPublishingRecoverer(kafkaTemplate(producerFactory()));
+
+        // 1초(1000MS) 간격으로 2회 재시도
+        FixedBackOff fixedBackOff = new FixedBackOff(1000L, 2L);
+        return new DefaultErrorHandler(recover, fixedBackOff);
     }
 }
