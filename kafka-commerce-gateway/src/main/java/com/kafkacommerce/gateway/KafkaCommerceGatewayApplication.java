3ac57809c1d0052d9ff4f29fbe36fbd4d6e57507
hyungjae.lee <dlgudwo11@gmail.com>
2025-05-07 16:47:51 +0900

    feat(gateway): spring cloud gateway 초기 설정

---
+++ kafka-commerce-gateway/src/main/java/com/kafkacommerce/gateway/KafkaCommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kafkacommerce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCommerceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCommerceGatewayApplication.class, args);
+    }
+
+}
